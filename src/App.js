7a1d19f9c0cb12f32930f88dc97d7d5a2187d05e
Yoowonjun <twojunspace@naver.com>
2023-05-05 21:45:03 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop management system.</h2>
-    </div>
-  );
+const customers = [{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '홍길동',
+  'birthday': '981229', 
+  'gender': '남자', 
+  'job': '대학생'
+}, 
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '홍길동',
+  'birthday': '981129', 
+  'gender': '남자', 
+  'job': '백엔드 개발자'
+}, 
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '홍길동',
+  'birthday': '981029', 
+  'gender': '남자', 
+  'job': '프론트엔드 개발자'
+}];
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        { customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} gender={c.gender} job={c.job} /> ); }) }
+      </div>
+    );
+  }
 }
 
 export default App;
