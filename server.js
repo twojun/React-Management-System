ce4fed01659b5748e5e49f8ca5ac76ee8c8af25d
Yoowonjun <twojunspace@naver.com>
2023-05-06 14:02:02 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,37 +7,30 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection =  mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
+
 // app.get('/api/hello', (req, res) => {
 //   res.send({message: 'hello express server!!'});
 // });
 
 app.get('/api/customers', (req, res) => {
-  res.send([
-      {
-        'id': 1,
-        'image': 'https://placeimg.com/64/64/1',
-        'name': '홍길동',
-        'birthday': '981229',
-        'gender': '남자',
-        'job': '대학생'
-      },
-      {
-        'id': 2,
-        'image': 'https://placeimg.com/64/64/2',
-        'name': '홍길동',
-        'birthday': '981129',
-        'gender': '남자',
-        'job': '백엔드 개발자'
-      },
-      {
-        'id': 3,
-        'image': 'https://placeimg.com/64/64/3',
-        'name': '홍길동',
-        'birthday': '981029',
-        'gender': '남자',
-        'job': '프론트엔드 개발자'
-      }
-  ]);
+  connection.query(
+    "SELECT * FROM CUSTOMER",
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
